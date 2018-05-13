fee9fcda8ae63405c19f761d118223ae65deddc6
albert <albertsanz92@gmail.com>
2018-05-13 23:44:21 +0200

    First Commit

--- src/main/java/BowlingGameTest.java
+++ src/main/java/BowlingGameTest.java
@@ -1,2 +1,7 @@
-public class BowlingGameTest {
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+    public void testGutterGame() throws Exception{
+        Game g = new Game();
+    }
 }
