7a2ffc436d643862b43ff8b82ceff72e390d0cad
albert <albertsanz92@gmail.com>
2018-05-17 22:31:32 +0200

    Seventeenth Commit

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -7,10 +7,10 @@ public class Game {
 
     public int score() {
         int score = 0;
-        for(int i=0; i< rolls.length; i++) {
-            if(rolls[i] + rolls[i+1]==10) //spare
-                score+=
-            score += rolls[i];
+        int i =0;
+        for(int frame=0; frame< 10; frame++) {
+            score += rolls[i]+rolls[i+1];
+            i+=2;
         }
         return score;
     }
